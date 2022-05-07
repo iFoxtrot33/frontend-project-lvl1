c38af2b8a76c71660b84bdb9cc9b54f625f1bdcf
iurii <furmaniurii@gmail.com>
2022-05-07 11:37:01 +0300

    Comments for brain-calc

--- src/cliCalc.js
+++ src/cliCalc.js
@@ -1,14 +1,14 @@
 import readlineSync from 'readline-sync';
 
 const calcGame = () => {
-  let firstNumber = Math.floor(Math.random() * 101);
-  let secondNumber = Math.floor(Math.random() * 101);
-  let sign = Math.floor(Math.random() * 3);
-  let ans = 0;
-  console.log('Welcome to the Brain Games!');
+  let firstNumber = Math.floor(Math.random() * 101);                           // First number with the range from 0 to 100
+  let secondNumber = Math.floor(Math.random() * 101);                         // Second number with the range from 0 to 100
+  let sign = Math.floor(Math.random() * 3);                                   // Range for math sign from 0 to 2 (+-*)
+  let ans = 0;                                                               // For users input 
+  console.log('Welcome to the Brain Games!');                                 //Greetings
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  if (sign === 0) {
+  console.log(`Hello, ${userName}!`); 
+  if (sign === 0) {                                                           //Chosing the sign of expression and taking user's answer
     console.log(`Question:${firstNumber} + ${secondNumber} = `);
     ans = readlineSync.question(`Your answer:`);
   } else if (sign === 1) {
@@ -19,7 +19,7 @@ const calcGame = () => {
     ans = readlineSync.question(`Your answer:`);
   }
   ans *= 1;
-  if ((sign === 0) && ((firstNumber + secondNumber) === ans)) {
+  if ((sign === 0) && ((firstNumber + secondNumber) === ans)) {                             //Deciding whether user's answer is correct or not
     console.log('Correct!');
   } else if ((sign === 0) && ((firstNumber + secondNumber) !== ans)) {
     return console.log(`${ans} is wrong answer ;(. Correct answer was ${firstNumber * secondNumber}.
@@ -36,8 +36,8 @@ const calcGame = () => {
   } else if ((sign === 2) && ((firstNumber * secondNumber) !== ans)) {
     return console.log(`${ans} is wrong answer ;(. Correct answer was ${firstNumber * secondNumber}.
         Let's try again,${userName}`);
-  }
-  firstNumber = Math.floor(Math.random() * 101);
+  }                                                                                      // First round completed. In case if the user is corret we continue the game
+  firstNumber = Math.floor(Math.random() * 101);                                         // Commence of the second round. 
   secondNumber = Math.floor(Math.random() * 101);
   sign = Math.floor(Math.random() * 3);
 
@@ -70,7 +70,7 @@ const calcGame = () => {
     return console.log(`${ans} is wrong answer ;(. Correct answer was ${firstNumber * secondNumber}.
         Let's try again,${userName}`);
   }
-  firstNumber = Math.floor(Math.random() * 101);
+  firstNumber = Math.floor(Math.random() * 101);                                                        //Commence of the third round
   secondNumber = Math.floor(Math.random() * 101);
   sign = Math.floor(Math.random() * 3);
 
@@ -103,7 +103,7 @@ const calcGame = () => {
     return console.log(`${ans} is wrong answer ;(. Correct answer was ${firstNumber * secondNumber}.
         Let's try again,${userName}`);
   }
-  return console.log(`Congratulations, ${userName}! `);
+  return console.log(`Congratulations, ${userName}! `);                                                // Final words in case of victory
 };
 
 export default calcGame;
