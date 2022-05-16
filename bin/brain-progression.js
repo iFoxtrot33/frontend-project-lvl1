4f9d0ee2dde510914002d62d0d7de819ac233786
iurii <furmaniurii@gmail.com>
2022-05-16 17:09:18 +0300

    Update gcd game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -5,18 +5,24 @@ import {
 
 import startGame from '../src/game-engine.js';
 
+const makeProgression = (step, randomLength, emptyArr, firstNumber) => {
+  let one = firstNumber;
+  for (let i = 0; i < randomLength; i += 1) {
+    emptyArr.push(one);
+    one += step;
+  }
+};
+
 const runGameProgression = () => {
   const gameRule = 'What number is missing in the progression?';
   let firstNumber = makeRandom(101);
   let step = makeRandomNoZero(10);
   const arrSteps = [6, 7, 8, 9, 10];
   let randomLength = arrSteps[Math.floor(Math.random() * (arrSteps.length))];
-  let progression = [];
+  const emptyArr = [];
+  let progression;
   let hiddenAnswer;
-  for (let i = 0; i < randomLength; i += 1) {
-    progression.push(firstNumber);
-    firstNumber += step;
-  }
+  progression = makeProgression((step, randomLength, emptyArr, firstNumber));
   hiddenAnswer = hiddenElement(randomLength) - 1;
   const ans1 = (progression[hiddenAnswer]).toString();
   progression[hiddenAnswer] = '..';
@@ -24,11 +30,7 @@ const runGameProgression = () => {
   randomLength = arrSteps[Math.floor(Math.random() * (arrSteps.length))];
   step = makeRandomNoZero(10);
   firstNumber = makeRandom(101);
-  progression = [];
-  for (let i = 0; i < randomLength; i += 1) {
-    progression.push(firstNumber);
-    firstNumber += step;
-  }
+  progression = makeProgression((step, randomLength, emptyArr, firstNumber));
   hiddenAnswer = hiddenElement(randomLength) - 1;
   const ans2 = (progression[hiddenAnswer]).toString();
   progression[hiddenAnswer] = '..';
@@ -36,11 +38,7 @@ const runGameProgression = () => {
   randomLength = arrSteps[Math.floor(Math.random() * (arrSteps.length))];
   step = makeRandomNoZero(10);
   firstNumber = makeRandom(101);
-  progression = [];
-  for (let i = 0; i < randomLength; i += 1) {
-    progression.push(firstNumber);
-    firstNumber += step;
-  }
+  progression = makeProgression((step, randomLength, emptyArr, firstNumber));
   hiddenAnswer = hiddenElement(randomLength) - 1;
   const ans3 = (progression[hiddenAnswer]).toString();
   progression[hiddenAnswer] = '..';
