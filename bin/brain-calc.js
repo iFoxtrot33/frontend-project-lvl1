60e14b6c2f56f33f7c6604d55cc925372aed2783
iurii <furmaniurii@gmail.com>
2022-05-16 16:44:25 +0300

    Update calc game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -5,6 +5,23 @@ import {
 
 import startGame from '../src/game-engine.js';
 
+const choseSign = (sign, one, two) => {
+  if (sign === 0) {
+    return `${one} + ${two}`;
+  } if (sign === 1) {
+    return `${one} - ${two}`;
+  }
+  return `${one} * ${two}`;
+};
+const choseSign1 = (sign, one, two) => {
+  if (sign === 0) {
+    return one + two;
+  } if (sign === 1) {
+    return one - two;
+  }
+  return one * two;
+};
+
 const runCalcGame = () => {
   const firstNumber = makeRandom(100);
   const secondNumber = makeRandom(100);
@@ -13,45 +30,15 @@ const runCalcGame = () => {
   const fifthNumber = makeRandom(100);
   const sixthNumber = makeRandom(100);
   let sign = makeRandom(3);
-  let first;
-  let second;
-  let third;
-  let ans1;
-  let ans2;
-  let ans3;
   const gameRule = 'What is the result of the expression?';
-  if (sign === 0) {
-    first = `${firstNumber} + ${secondNumber}`;
-    ans1 = (firstNumber + secondNumber).toString();
-  } else if (sign === 1) {
-    first = `${firstNumber} - ${secondNumber}`;
-    ans1 = (firstNumber - secondNumber).toString();
-  } else if (sign === 2) {
-    first = `${firstNumber} * ${secondNumber}`;
-    ans1 = (firstNumber * secondNumber).toString();
-  }
+  const first = choseSign(sign, firstNumber, secondNumber);
+  const ans1 = (choseSign1(sign, firstNumber, secondNumber)).toString();
   sign = makeRandom(3);
-  if (sign === 0) {
-    second = `${thirdNumber} + ${fourthNumber}`;
-    ans2 = (thirdNumber + fourthNumber).toString();
-  } else if (sign === 1) {
-    second = `${thirdNumber} - ${fourthNumber}`;
-    ans2 = (thirdNumber - fourthNumber).toString();
-  } else if (sign === 2) {
-    second = `${thirdNumber} * ${fourthNumber}`;
-    ans2 = (thirdNumber * fourthNumber).toString();
-  }
+  const second = choseSign(sign, thirdNumber, fourthNumber);
+  const ans2 = (choseSign1(sign, thirdNumber, fourthNumber)).toString();
   sign = makeRandom(3);
-  if (sign === 0) {
-    third = `${fifthNumber} + ${sixthNumber}`;
-    ans3 = (fifthNumber + sixthNumber).toString();
-  } else if (sign === 1) {
-    third = `${fifthNumber} - ${sixthNumber}`;
-    ans3 = (fifthNumber - sixthNumber).toString();
-  } else if (sign === 2) {
-    third = `${fifthNumber} * ${sixthNumber}`;
-    ans3 = (fifthNumber * sixthNumber).toString();
-  }
+  const third = choseSign(sign, fifthNumber, sixthNumber);
+  const ans3 = (choseSign1(sign, fifthNumber, sixthNumber)).toString();
   return startGame(first, second, third, ans1, ans2, ans3, gameRule);
 };
 console.log(runCalcGame());
