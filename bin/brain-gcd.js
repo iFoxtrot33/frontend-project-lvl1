a68c76eaa2686244c1707aea0257a9117e22b828
iurii <furmaniurii@gmail.com>
2022-05-16 16:56:25 +0300

    Update gcd game

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,49 +5,40 @@ import {
 
 import startGame from '../src/game-engine.js';
 
+const findCommonDividers = (arr1, arr2, bothArr) => {
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr2.includes(arr1[i]) === true) {
+      bothArr.push(arr1[i]);
+    }
+  }
+  return bothArr;
+};
+
 const runGameGcd = () => {
   const gameRule = 'Find the greatest common divisor of given numbers.';
   let firstNumber = makeRandomNoZero(100);
   let secondNumber = makeRandomNoZero(100);
-  let firstArr = [];
-  let secondArr = [];
   let bothArr = [];
-  firstArr = calcDividers(firstNumber);
-  secondArr = calcDividers(secondNumber);
-  for (let i = 0; i < firstArr.length; i += 1) {
-    if (secondArr.includes(firstArr[i]) === true) {
-      bothArr.push(firstArr[i]);
-    }
-  }
-  const ans1 = (findMax(bothArr)).toString();
+  let firstArr = calcDividers(firstNumber);
+  let secondArr = calcDividers(secondNumber);
+  let dividers = findCommonDividers(firstArr, secondArr, bothArr);
+  const ans1 = (findMax(dividers)).toString();
   const first = `${firstNumber} ${secondNumber}`;
   firstNumber = makeRandomNoZero(100);
   secondNumber = makeRandomNoZero(100);
-  firstArr = [];
-  secondArr = [];
   firstArr = calcDividers(firstNumber);
   secondArr = calcDividers(secondNumber);
   bothArr = [];
-  for (let i = 0; i < firstArr.length; i += 1) {
-    if (secondArr.includes(firstArr[i]) === true) {
-      bothArr.push(firstArr[i]);
-    }
-  }
-  const ans2 = (findMax(bothArr)).toString();
+  dividers = findCommonDividers(firstArr, secondArr, bothArr);
+  const ans2 = (findMax(dividers)).toString();
   const second = `${firstNumber} ${secondNumber}`;
   firstNumber = makeRandomNoZero(100);
   secondNumber = makeRandomNoZero(100);
-  firstArr = [];
-  secondArr = [];
   firstArr = calcDividers(firstNumber);
   secondArr = calcDividers(secondNumber);
   bothArr = [];
-  for (let i = 0; i < firstArr.length; i += 1) {
-    if (secondArr.includes(firstArr[i]) === true) {
-      bothArr.push(firstArr[i]);
-    }
-  }
-  const ans3 = (findMax(bothArr)).toString();
+  dividers = findCommonDividers(firstArr, secondArr, bothArr);
+  const ans3 = (findMax(dividers)).toString();
   const third = `${firstNumber} ${secondNumber}`;
   return startGame(first, second, third, ans1, ans2, ans3, gameRule);
 };
