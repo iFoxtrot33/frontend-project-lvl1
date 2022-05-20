54f2b8060b396f8aa8362acb1f8d2adf7d31fa46
iurii <furmaniurii@gmail.com>
2022-05-20 10:16:50 +0300

    Update calc game

--- src/games/cliCalc.js
+++ src/games/cliCalc.js
@@ -5,12 +5,8 @@ import {
 import startGame from '../game-engine.js';
 
 const choseSign = (sign, one, two) => {
-  if (sign === 0) {
-    return `${one} + ${two}`;
-  } if (sign === 1) {
-    return `${one} - ${two}`;
-  }
-  return `${one} * ${two}`;
+  const signValue = ['+', '-', '*'];
+  return `${one} ${signValue[sign]} ${two}`;
 };
 const choseSign1 = (sign, one, two) => {
   if (sign === 0) {
@@ -28,7 +24,7 @@ const runCalcGame = () => {
   for (let i = 0; i < 3; i += 1) {
     const firstNumber = getRandomNumber(0, 100);
     const secondNumber = getRandomNumber(0, 100);
-    const sign = getRandomNumber(0, 3);
+    const sign = getRandomNumber(0, 2);
     number.push(choseSign(sign, firstNumber, secondNumber));
     expectedAnswer.push((choseSign1(sign, firstNumber, secondNumber)).toString());
   }
