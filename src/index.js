47a9a9665350dd0692bcd9be7453f021f2eff785
iurii <furmaniurii@gmail.com>
2022-05-27 05:40:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const runGreetings = () => {
+const startGame = (gameData, gamerule) => {
+  let clientAnswer;
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-const askGameQuestion = (question) => {
-  console.log(`Question: ${question}`);
-};
-
-const askAnswer = () => readlineSync.question('Your answer: ');
-
-const runWrongAnswer = (clientAnswer, userName, correctAnswer) => {
-  console.log(`${clientAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.
-Let's try again, ${userName}!`);
-};
-
-export const runVictory = (userName) => {
-  console.log(`Congratulations, ${userName}!`);
-};
-
-const startGame = (gameData, gamerule) => {
-  let clientAnswer;
-  const userName = (runGreetings());
   console.log(gamerule);
   for (let i = 0; i < gameData.length; i += 2) {
-    askGameQuestion(gameData[i]);
-    clientAnswer = askAnswer();
+    console.log(`Question: ${gameData[i]}`);
+    clientAnswer = readlineSync.question('Your answer: ');
     if ((clientAnswer) === (gameData[i + 1])) {
       console.log('Correct!');
     } else {
-      runWrongAnswer(clientAnswer, userName, gameData[i + 1]);
+      console.log(`${clientAnswer} is wrong answer ;(. Correct answer was ${gameData[i + 1]}.
+        Let's try again, ${userName}!`);
       break;
     }
     if (i === 4) {
-      runVictory(userName);
+      console.log(`Congratulations, ${userName}!`);
     }
   }
 };
