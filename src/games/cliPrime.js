d2eda7f82cfe144d613f2737a3138e250250b5cb
iurii <furmaniurii@gmail.com>
2022-05-27 06:50:38 +0300

    Update prime game

--- src/games/cliPrime.js
+++ src/games/cliPrime.js
@@ -2,30 +2,26 @@ import getRandomNumber from '../helpers.js';
 
 import startGame from '../index.js';
 
-const calcDividers = (num) => {
+const isPrime = (num) => {
+  const requiredDividers = 2;
   const arr = [];
   for (let k = 0; k <= num; k += 1) {
     if ((num % k) === 0) {
       arr.push(k);
     }
   }
-  return arr;
-};
-
-const isPrime = (num) => {
-  const arr = calcDividers(num);
-  return (arr.length === 2);
+  return (arr.length === requiredDividers);
 };
 
 const runGamePrime = () => {
   const gameData = [];
   let gameRound = [];
-  let first;
+  let number;
   const gamerule = '"yes" if given number is prime. Otherwise answer "no"';
   for (let i = 0; i < 3; i += 1) {
-    first = getRandomNumber(1, 1000);
-    gameRound.push(first);
-    gameRound.push(isPrime(first) ? 'yes' : 'no');
+    number = getRandomNumber(1, 1000);
+    gameRound.push(number);
+    gameRound.push(isPrime(number) ? 'yes' : 'no');
     gameData.push(gameRound);
     gameRound = [];
   }
