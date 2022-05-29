8def0ce967d157cd9c1f3257e894e3bd382cf93d
iurii <furmaniurii@gmail.com>
2022-05-29 03:50:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,26 +4,24 @@ const startGame = (gameData, gamerule) => {
   let clientAnswer;
   const gameQuestion = 0;
   const rightAnswer = 1;
-  const requiredVictories = 3;
+  const requiredVictories = 2;
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gamerule);
-  let winsCounter = 0;
   for (let round = 0; round < gameData.length; round += 1) {
     console.log(`Question: ${gameData[round][gameQuestion]}`);
     clientAnswer = readlineSync.question('Your answer: ');
-    if ((clientAnswer) === (gameData[round][rightAnswer])) {
-      console.log('Correct!');
-    } else {
+    if ((clientAnswer) !== (gameData[round][rightAnswer])) {
       console.log(`${clientAnswer} is wrong answer ;(. Correct answer was ${gameData[round][rightAnswer]}.
         Let's try again, ${userName}!`);
       break;
+    } else {
+      console.log('Correct!');
+      if (round === requiredVictories) {
+        console.log(`Congratulations, ${userName}!`);
+      }
     }
-    winsCounter += 1;
-  }
-  if (winsCounter === requiredVictories) {
-    console.log(`Congratulations, ${userName}!`);
   }
 };
 
